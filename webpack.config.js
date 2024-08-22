72b1609d015a29befd129f56f597891fb3a298f1
Esteban Córdoba <esteban.cordoba@moove-it.com>
2024-08-22 08:26:07 -0500

    webpack initial configuration

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const { ModuleFederationPlugin } = require('webpack').container;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3001,
+  },
+  output: {
+    publicPath: 'auto',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'ReactApp',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './ReactApp': './src/App',
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
